eeba81666a5db6f04110360bbb45fdf90afff761
Shamar <Shamar098@gmail.com>
2024-12-05 15:55:22 +0000

    refactor: made getAllAlbums take in new variables for Artist (price, stock)

--- src/test/java/com/northcoders/jv_record_shop/controller/RecordShopControllerTests.java
+++ src/test/java/com/northcoders/jv_record_shop/controller/RecordShopControllerTests.java
@@ -40,32 +40,35 @@ class RecordShopControllerTests {
     private ObjectMapper mapper;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         mockMvcController = MockMvcBuilders.standaloneSetup(recordShopController).build();
         mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
     }
 
     @Test
     @DisplayName("GET all the albums in the database")
     void getAllAlbums_Test() throws Exception {
-        Artist testArtist = new Artist(1L,"THE Artist");
+        Artist testArtist = new Artist(1L, "THE Artist");
 
-        Album testAlbum1 = new Album(1L,"testOne",testArtist,Genre.JAZZ,LocalDate.now());
-        Album testAlbum2 = new Album(2L,"testTwo",testArtist,Genre.POP,LocalDate.now());
-        Album testAlbum3 = new Album(3L,"testThree",testArtist,Genre.UNKNOWN,LocalDate.now());
+        Album testAlbum1 = new Album(1L, "testOne", testArtist, Genre.JAZZ, LocalDate.now(),5,10.50);
+        Album testAlbum2 = new Album(2L, "testTwo", testArtist, Genre.POP, LocalDate.now(),2,12.12);
+        Album testAlbum3 = new Album(3L, "testThree", testArtist, Genre.UNKNOWN, LocalDate.now(),9,5.0);
 
-        List<Album> albumList = List.of(testAlbum1,testAlbum2,testAlbum3);
+        List<Album> albumList = List.of(testAlbum1, testAlbum2, testAlbum3);
 
         Mockito.when(mockRecordShopServiceImpl.getAllAlbums()).thenReturn(albumList);
 
+
         this.mockMvcController.perform(
-                MockMvcRequestBuilders.get("/api/v1/recordshop"))
+                        MockMvcRequestBuilders.get("/api/v1/recordshop"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1L))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("testOne"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].stock").value("5"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].genre").value("POP"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("testTwo"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[2].genre").value("UNKNOWN"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[2].name").value("testThree"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].artist.name").value("THE Artist"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].price").value("5.0"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].releaseDate").value("05-12-2024"));
     }
 }

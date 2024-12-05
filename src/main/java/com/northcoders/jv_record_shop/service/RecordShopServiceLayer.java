5d8b4baa5e572c4f956d2c556c31b74290f97e8f
Shamar <Shamar098@gmail.com>
2024-12-05 14:09:40 +0000

    feat: add methods to RecordShopServiceLayer

--- src/main/java/com/northcoders/jv_record_shop/service/RecordShopServiceLayer.java
+++ src/main/java/com/northcoders/jv_record_shop/service/RecordShopServiceLayer.java
@@ -1,4 +1,15 @@
 package com.northcoders.jv_record_shop.service;
 
+import com.northcoders.jv_record_shop.model.Album;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public interface RecordShopServiceLayer {
+    List<Album> getAllAlbums();
+    Album getAlbumById(String id);
+    Album addAlbum(Album album);
+    Album updateAlbumDetails(String id,Album album);
+    boolean deleteAlbumById(String id);
 }
--- src/main/java/com/northcoders/jv_record_shop/service/RecordShopServiceLayerImpl.java
+++ src/main/java/com/northcoders/jv_record_shop/service/RecordShopServiceLayerImpl.java
@@ -1,4 +1,41 @@
 package com.northcoders.jv_record_shop.service;
 
+import com.northcoders.jv_record_shop.model.Album;
+import com.northcoders.jv_record_shop.repository.RecordShopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RecordShopServiceLayerImpl implements RecordShopServiceLayer {
+    @Autowired
+    RecordShopRepository recordShopRepository;
+
+    @Override
+    public List<Album> getAllAlbums() {
+        return null;
+    }
+
+    @Override
+    public Album getAlbumById(String id) {
+        return null;
+    }
+
+    @Override
+    public Album addAlbum(Album album) {
+        return null;
+    }
+
+    @Override
+    public Album updateAlbumDetails(String id, Album album) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteAlbumById(String id) {
+        return false;
+    }
+
 }

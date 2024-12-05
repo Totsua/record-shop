f1fca75db5fbcac8d4988470db920fa4f4a5e63f
Shamar <Shamar098@gmail.com>
2024-12-05 11:51:11 +0000

    feat: add RecordShopController

---
+++ src/main/java/com/northcoders/jv_record_shop/controller/RecordShopController.java
@@ -0,0 +1,48 @@
+package com.northcoders.jv_record_shop.controller;
+
+import com.northcoders.jv_record_shop.model.Album;
+import com.northcoders.jv_record_shop.service.RecordShopServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/recordshop")
+public class RecordShopController {
+
+    @Autowired
+    RecordShopServiceLayer recordShopService;
+
+    @GetMapping
+    public ResponseEntity<List<Album>> getAllAlbums(){
+
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Album> getAlbumById(@PathVariable String id){
+
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<Album> addAlbum(@RequestBody Album album){
+
+        return null;
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Album> updateAlbumDetails(@PathVariable String id, @RequestBody Album album){
+
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Album> deleteAlbumByID(@PathVariable String id){
+
+        return null;
+    }
+
+}

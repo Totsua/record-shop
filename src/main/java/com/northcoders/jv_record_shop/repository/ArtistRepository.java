15071465cad37d1d308fc57d6b50695e9f034100
Shamar <Shamar098@gmail.com>
2024-12-09 01:53:52 +0000

    feat: add repository for Artist

---
+++ src/main/java/com/northcoders/jv_record_shop/repository/ArtistRepository.java
@@ -0,0 +1,7 @@
+package com.northcoders.jv_record_shop.repository;
+
+import com.northcoders.jv_record_shop.model.Artist;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArtistRepository extends CrudRepository<Artist,Long> {
+}

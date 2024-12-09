b423089209378028902f8fbf1afe8cc7bc79a6b1
Shamar <Shamar098@gmail.com>
2024-12-09 15:03:42 +0000

    feat: add caching to Repository / logging

--- src/main/java/com/northcoders/jv_record_shop/repository/RecordShopRepository.java
+++ src/main/java/com/northcoders/jv_record_shop/repository/RecordShopRepository.java
@@ -1,9 +1,21 @@
 package com.northcoders.jv_record_shop.repository;
 
 import com.northcoders.jv_record_shop.model.Album;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RecordShopRepository extends CrudRepository<Album,Long> {
+    @Override
+    @CacheEvict(cacheNames = "albums", key = "#result.id")
+    <S extends Album> S save (S s);
+
+    @Override
+    @Cacheable("albums")
+    Optional<Album> findById(Long aLong);
+
 }

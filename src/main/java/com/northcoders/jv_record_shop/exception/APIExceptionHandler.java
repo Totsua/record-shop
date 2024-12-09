db11fe88ac35ed08f19466ceca526a7e9af8f1ff
Shamar <Shamar098@gmail.com>
2024-12-09 01:58:47 +0000

    feat: add exception handler for TransactionSystemException

--- src/main/java/com/northcoders/jv_record_shop/exception/APIExceptionHandler.java
+++ src/main/java/com/northcoders/jv_record_shop/exception/APIExceptionHandler.java
@@ -1,17 +1,20 @@
 package com.northcoders.jv_record_shop.exception;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @RestControllerAdvice
 public class APIExceptionHandler {
@@ -40,5 +43,21 @@ public class APIExceptionHandler {
       fieldErrors.forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
       return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+   }
+
+   // ConstraintViolationException is being thrown wrapped in a TransactionSystemException
+   @ExceptionHandler(TransactionSystemException.class)
+   public ResponseEntity<String> handleTransactionSystemException(final TransactionSystemException e){
+
+      if(e.getRootCause() instanceof ConstraintViolationException constraints){
+          Set<ConstraintViolation<?>> violations = constraints.getConstraintViolations();
+
+         List<String> violationMessages = violations.stream()
+                 .map(ConstraintViolation::getMessage)
+                 .toList();
+         return new ResponseEntity<>("There are errors: "+ violationMessages,HttpStatus.BAD_REQUEST);
+
+      }
+      return new ResponseEntity<>(e.getRootCause().getMessage(),HttpStatus.BAD_REQUEST);
    }
 }

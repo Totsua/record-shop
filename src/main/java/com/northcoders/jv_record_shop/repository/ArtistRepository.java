6591d099624835941ce1c855c472ed2d1ba65d76
Shamar <Shamar098@gmail.com>
2024-12-26 17:02:23 +0000

    feat: add find artist by name query

--- src/main/java/com/northcoders/jv_record_shop/repository/ArtistRepository.java
+++ src/main/java/com/northcoders/jv_record_shop/repository/ArtistRepository.java
@@ -3,5 +3,8 @@ package com.northcoders.jv_record_shop.repository;
 import com.northcoders.jv_record_shop.model.Artist;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ArtistRepository extends CrudRepository<Artist,Long> {
+    Optional<Artist> findByName(String name);
 }

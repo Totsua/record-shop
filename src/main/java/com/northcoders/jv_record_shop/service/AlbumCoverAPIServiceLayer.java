bc491fcf4d20e662eecd4248a4cf2d1a19a4825e
Shamar <Shamar098@gmail.com>
2025-01-03 19:00:56 +0000

    feat: add Service layer and implementation for itunes api

---
+++ src/main/java/com/northcoders/jv_record_shop/service/AlbumCoverAPIServiceLayer.java
@@ -0,0 +1,7 @@
+package com.northcoders.jv_record_shop.service;
+
+import com.northcoders.jv_record_shop.model.Album;
+
+public interface AlbumCoverAPIServiceLayer {
+    String findAlbumCoverURL(String albumName);
+}
---
+++ src/main/java/com/northcoders/jv_record_shop/service/AlbumCoverAPIServiceLayerImpl.java
@@ -0,0 +1,10 @@
+package com.northcoders.jv_record_shop.service;
+
+import com.northcoders.jv_record_shop.model.Album;
+
+public class AlbumCoverAPIServiceLayerImpl implements AlbumCoverAPIServiceLayer{
+    @Override
+    public String findAlbumCoverURL(String albumName) {
+        return null;
+    }
+}

2eea1f85ff7e7c442019e2ef778c0e52979dbd46
Shamar <Shamar098@gmail.com>
2025-01-03 20:57:56 +0000

    refactor: remove unnecessary imports

--- src/main/java/com/northcoders/jv_record_shop/service/AlbumCoverAPIServiceLayer.java
+++ src/main/java/com/northcoders/jv_record_shop/service/AlbumCoverAPIServiceLayer.java
@@ -1,7 +1,5 @@
 package com.northcoders.jv_record_shop.service;
 
-import com.northcoders.jv_record_shop.model.Album;
-
 public interface AlbumCoverAPIServiceLayer {
     String findAlbumCoverURL(String albumName);
 }
